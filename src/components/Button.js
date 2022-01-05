6054c77968c541d7442b5abf7d2e8e9e39c74873
Soulaiman Meskini <65052301+SoulaimanMeskini@users.noreply.github.com>
2022-01-05 12:30:48 +0100

    Co-authored-by: Twan L <twanluttik@gmail.com>

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,12 @@ import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 export const Button = (p) => {
-
-
-    return <ButtonBody >
-        <
-        p > { p.text } < /p> <
-        /ButtonBody>
+  return <ButtonBody>
+    <p>{p.text}</p>
+  </ButtonBody>
 }
 
-
-const ButtonBody = styled.div `
+const ButtonBody = styled.div`
   padding: 10px;
   cursor: pointer;
 `;
