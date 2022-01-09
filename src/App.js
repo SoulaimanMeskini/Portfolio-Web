59a89689eeee2df37d1798d0dfe27da9da3b2184
Soulaiman Meskini <65052301+SoulaimanMeskini@users.noreply.github.com>
2022-01-09 20:11:00 +0100

    Circle lens update
    Addded a Cricle lens with a test gif

--- src/App.js
+++ src/App.js
@@ -1,104 +1,77 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import media from './assets/gif/test.gif';
 
 export const App = () => {
-  return <AppBody>
-    <Container>
-      <Row>
-        <TopLeft />
-        <TopMiddle />
-        <TopRight />
-      </Row>
-      <Row style={{ height: '100%' }}>
-        <MiddleLeft /> 
-        <MainCircle />
-        <MiddleRight />
-      </Row>
-      <Row>
-        <BottomLeft />
-        <BottomMiddle />
-        <BottomRight />
-      </Row>      
-    </Container>
+  const [FullScreen, SetFullScreen] = useState(0);
+
+  useEffect(() => {
+    },[]);
+return <AppBody>
+    <ImgCircle src={media} onClick={() => SetFullScreen(!FullScreen)} s={FullScreen} />
   </AppBody>
 }
 
 
+
+const ImgCircle = styled.img`
+  height: ${({ s }) => s ? 100: 50}vh;
+  width: ${({ s }) => s ? 100: 50}vh;
+  border-radius: 10000px;
+  object-fit: cover;
+  
+
+  transition: height .1s ease-in-out, width .1s ease-in-out;
+`;
+
+
+const Overlay = styled.div`
+ position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 5;
+  background-color: black;
+`;
 const AppBody = styled.div`
   height: 100%;
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  align-items: center;
   background-color: black;
 `;
 
-const Container = styled.div`
-  width: fit-content;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
+const FullView = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 100%;
-    width: 100%;
+  z-index: 10;
+  background-color: transparent;
 `;
 
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  > div  {
-
-    &:nth-child(1) {
-      background-color: red!important;
-    };
-
-    &:nth-child(2) {
-      height: 100%;
-      background-color: green!important;
-    };
-    
-    &:nth-child(3) {
-      background-color: yellow!important;
-    };
-
-    height: 200px;
-    width: 200px;
+const Btn = styled.button`
+  padding: 8px 12px;
+  border-radius: 8px;
+  background-color: #B6B6B6;
+  border: none;
+  
+  &:hover {
+    opacity: 0.6;    
   }
 `;
 
-const TopLeft = styled.div`
-  border-left: 1px solid white;
-  border-bottom: 1px solid white;
-`
-const TopMiddle =styled.div`
-  height: 100px;
+const Circle = styled.div`
+  height: 20vh;
+  width: 20vh;
+  border-radius: 500px;
+  cursor: pointer;
+  img {
+    height: 100%;
+     width: 100%;
+      border-radius: 500px;
+  }
 `;
-
-const TopRight = styled.div``
-const MiddleLeft = styled.div`
-  height:100%;
-`
-const MiddleRight = styled.div`
-  height:100%;
-  `
-const BottomLeft = styled.div``
-const BottomMiddle = styled.div`
-  height: 100px;
-`
-const BottomRight=styled.div``
-
-
-
-
-const MainCircle  = () => {
-  return <MainCircleBody>
-
-  </MainCircleBody>
-}
-
-const MainCircleBody= styled.div`
-  height: 500px!important;
-  width: 500px!important;
-  border-radius: 100%;
-  background-color: white;
-`;
-
-
-
