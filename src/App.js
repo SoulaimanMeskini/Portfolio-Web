edb3956fc8136e3c82b45dd4d7e9ac3eface26a6
Soulaiman Meskini <65052301+SoulaimanMeskini@users.noreply.github.com>
2022-01-09 23:16:51 +0100

    Lens circle maded
    Lens circle done and a slight beginning for the lines animation

--- src/App.js
+++ src/App.js
@@ -3,75 +3,28 @@ import { useEffect, useState } from 'react';
 import media from './assets/gif/test.gif';
 
 export const App = () => {
-  const [FullScreen, SetFullScreen] = useState(0);
+  
+  function toggleClasses(){
+    var img = document.getElementsByTagName("img")[0].classList.add("transitionGif");
+  }
 
-  useEffect(() => {
-    },[]);
-return <AppBody>
-    <ImgCircle src={media} onClick={() => SetFullScreen(!FullScreen)} s={FullScreen} />
-  </AppBody>
+  return (
+    <>
+      <div class="WhiteText">Made by Ming Li Janssen</div>
+      <ImgCircle src={media} onClick={toggleClasses}>
+      </ImgCircle>
+      <div class="Black Top"></div>
+      <div class="Black Left"></div>
+      <div class="Black Right"></div>
+      <div class="Black Bot"></div>
+    </>
+  )
 }
 
-
-
 const ImgCircle = styled.img`
-  height: ${({ s }) => s ? 100: 50}vh;
-  width: ${({ s }) => s ? 100: 50}vh;
-  border-radius: 10000px;
-  object-fit: cover;
-  
-
-  transition: height .1s ease-in-out, width .1s ease-in-out;
-`;
-
-
-const Overlay = styled.div`
- position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 5;
-  background-color: black;
-`;
-const AppBody = styled.div`
-  height: 100%;
-  width: 100%;
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  background-color: black;
-`;
-
-const FullView = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 10;
-  background-color: transparent;
-`;
-
-const Btn = styled.button`
-  padding: 8px 12px;
-  border-radius: 8px;
-  background-color: #B6B6B6;
-  border: none;
-  
-  &:hover {
-    opacity: 0.6;    
-  }
-`;
-
-const Circle = styled.div`
-  height: 20vh;
-  width: 20vh;
-  border-radius: 500px;
+  height: 100vh;
+  width: 100vW;
+  clip-path: circle(20% at 50% 50%);
+  transition: clip-path 1s;
   cursor: pointer;
-  img {
-    height: 100%;
-     width: 100%;
-      border-radius: 500px;
-  }
 `;
